13495430fefd49d9cea77cc6b82d807521243584
Frederick Thomas <supercodingninja@outlook.com>
2017-06-21 14:29:14 -0700

    localStorage

--- app.js
+++ app.js
@@ -18,26 +18,32 @@ function Image(name, filepath) {
 }
 
 //object instances
-var bag = new Image('bag', 'img/bag.jpg');
-var banana = new Image('banana', 'img/banana.jpg');
-var bathroom = new Image('bathroom', 'img/bathroom.jpg');
-var boots = new Image('boots', 'img/boots.jpg');
-var breakfast = new Image('breakfast', 'img/breakfast.jpg');
-var bubblegum = new Image('bugglegum', 'img/bubblegum.jpg');
-var chair = new Image('chair', 'img/chair.jpg');
-var cthulhu = new Image('cthulhu', 'img/cthulhu.jpg');
-var dogDuck = new Image('dog-duck', 'img/dog-duck.jpg');
-var dragon = new Image('dragon', 'img/dragon.jpg');
-var pen = new Image('pen', 'img/pen.jpg');
-var petSweep = new Image('pet-sweep', 'img/pet-sweep.jpg');
-var scissors = new Image('scissors', 'img/scissors.jpg');
-var shark = new Image('shark', 'img/shark.jpg');
-var sweep = new Image('sweep', 'img/sweep.png');
-var tauntaun = new Image('tauntaun', 'img/tauntaun.jpg');
-var unicorn = new Image('unicorn', 'img/unicorn.jpg');
-var usb = new Image('usb', 'img/usb.gif');
-var waterCan = new Image('water-can', 'img/water-can.jpg');
-var wineGlass = new Image('wine-glass', 'img/wine-glass.jpg');
+if(localStorage.data){
+  allImages = JSON.parse(localStorage.data);
+} else {
+//   var allImages = [];
+
+  var bag = new Image('bag', 'img/bag.jpg');
+  var banana = new Image('banana', 'img/banana.jpg');
+  var bathroom = new Image('bathroom', 'img/bathroom.jpg');
+  var boots = new Image('boots', 'img/boots.jpg');
+  var breakfast = new Image('breakfast', 'img/breakfast.jpg');
+  var bubblegum = new Image('bugglegum', 'img/bubblegum.jpg');
+  var chair = new Image('chair', 'img/chair.jpg');
+  var cthulhu = new Image('cthulhu', 'img/cthulhu.jpg');
+  var dogDuck = new Image('dog-duck', 'img/dog-duck.jpg');
+  var dragon = new Image('dragon', 'img/dragon.jpg');
+  var pen = new Image('pen', 'img/pen.jpg');
+  var petSweep = new Image('pet-sweep', 'img/pet-sweep.jpg');
+  var scissors = new Image('scissors', 'img/scissors.jpg');
+  var shark = new Image('shark', 'img/shark.jpg');
+  var sweep = new Image('sweep', 'img/sweep.png');
+  var tauntaun = new Image('tauntaun', 'img/tauntaun.jpg');
+  var unicorn = new Image('unicorn', 'img/unicorn.jpg');
+  var usb = new Image('usb', 'img/usb.gif');
+  var waterCan = new Image('water-can', 'img/water-can.jpg');
+  var wineGlass = new Image('wine-glass', 'img/wine-glass.jpg');
+}
 
 function displayImg(index) {
   var img1 = document.createElement('IMG');
@@ -129,6 +135,7 @@ function handleClick() {
     container.innerHTML = '';
     displayAll();
   }
+  localStorage.data = JSON.stringify(allImages); // WORKS //
 }
 
 //Show Results
